a9b31d8f37332b28fc53e3d1e6e42220426138d7
Ed Orsini <edorsini@mit.edu>
2017-10-03 18:07:47 -0400

    Changed indenting to 2 spaces.

--- server/app/models/resource.js
+++ server/app/models/resource.js
@@ -8,13 +8,13 @@ module.exports = function(sequelize, DataTypes) {
       }
     },
     description: {
-      type: DataTypes.TEXT,
+      type: DataTypes.TEXT, 
       allowNull: true,
     },
     link: {
       type: DataTypes.STRING,
       allowNull: false,
-    },
+    }, 
   }, {
     classMethods: {
       associate: function(models) {
