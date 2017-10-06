bab4614a1802e2b5d328928bc29160177ba77619
Walter Augustine <walter.augustine10@gmail.com>
2017-10-05 23:09:04 -0400

    add oauth2 package. return access_token on successful auth

--- server/app/controllers/api.js
+++ server/app/controllers/api.js
@@ -2,14 +2,56 @@ const express = require('express');
 const router = express.Router();
 const db = require('../models');
 const path = require('path');
+const simpleOauthModule = require('simple-oauth2');
 
 module.exports = function (app) {
   app.use('/', router);
 };
 
-router.get('/', function (req, res, next) {
+// Initialize the OAuth2 Library
+const oauth2 = simpleOauthModule.create({
+  client: {
+    id: process.env.GH_CLIENT_ID,
+    secret: process.env.GH_SECRET
+  },
+  auth: {
+    tokenHost: 'https://github.com',
+    tokenPath: '/login/oauth/access_token',
+    authorizePath: '/login/oauth/authorize'
+  }
+});
+
+// Authorization oauth2 URI
+const authorizationUri = oauth2.authorizationCode.authorizeURL({
+  redirect_uri: 'http://localhost:3000/callback',
+  scope: 'user:email'
+});
+
+router.get('/auth', function (req, res, next) {
   // pass the github tokens to the client
-  console.log('hello from root!');
+  console.log(authorizationUri);
+  res.redirect(authorizationUri);
+});
+
+router.get('/callback', function (req, res, next) {
+  const code = req.query.code;
+  const options = {
+    code
+  };
+
+  oauth2.authorizationCode.getToken(options, (error, result) => {
+    if (error) {
+      console.error('Access Token Error', error.message);
+      return res.json('Authentication failed');
+    }
+
+    result.expires_at = Date.now() * 100;
+    const token = oauth2.accessToken.create(result);
+  });
+});
+
+router.get('/login', (req, res) => {
+  res.send('Hello<br><a href="/auth">Log in with Github</a>');
 });
 
 router.get('/api/', function (req, res, next) {
