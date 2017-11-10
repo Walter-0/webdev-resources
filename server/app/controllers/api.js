e215684c601a905d41988f076ee6da624358d49a
Walter Augustine <walter.augustine10@gmail.com>
2017-11-10 10:20:41 -0500

    login with github working

--- server/app/controllers/api.js
+++ server/app/controllers/api.js
@@ -2,82 +2,42 @@ const express = require('express');
 const router = express.Router();
 const db = require('../models');
 const path = require('path');
-const request = require('request');
-const simpleOauthModule = require('simple-oauth2');
+const passport = require('passport');
 
 module.exports = function (app) {
   app.use('/', router);
 };
 
-// Initialize the OAuth2 Library
-const oauth2 = simpleOauthModule.create({
-  client: {
-    id: process.env.GH_CLIENT_ID,
-    secret: process.env.GH_SECRET
-  },
-  auth: {
-    tokenHost: 'https://github.com',
-    tokenPath: '/login/oauth/access_token',
-    authorizePath: '/login/oauth/authorize'
-  }
-});
-
-// Authorization oauth2 URI
-const authorizationUri = oauth2.authorizationCode.authorizeURL({
-  redirect_uri: 'http://localhost:3000/callback',
-  scope: 'user:email'
-});
-
-router.get('/auth', function (req, res) {
-  res.redirect(authorizationUri);
-});
-
-router.get('/callback', function (req, res, next) {
-  let endpoint;
-  const code = req.query.code;
-  const options = {
-    code
-  };
-
-  oauth2.authorizationCode.getToken(options)
-    .then((result) => {
-      result.expires_at = Date.now() * 100;
-      return oauth2.accessToken.create(result);
-    })
-    .then((token) => {
-      const options = {
-        'url': `https://api.github.com/user?access_token=${token.token.access_token}`,
-        'json': true,
-        'headers': {
-          'Authorization': `Bearer ${token.token.access_token}`,
-          'User-Agent': 'webdev-resources',
-          'Accept': 'application/vnd.github.v3+json'
-        }
-      };
-      request
-        .get(options)
-        .on('response', function (githubResponse) {
-          endpoint = githubResponse.request.uri.href;
-          // we need to make a GET request to this endpoint and get user data
-        });
-      console.log(endpoint);
-    })
-    .catch((error) => {
-      console.log('Access Token Error', error.message);
-    });
-});
-
-router.get('/hello', function (req, res) {
-  res.render('hello', {
-    name: 'bilbo',
-    token: 'your token here',
-    email: 'your github email'
+router.get('/login', function (req, res) {
+  res.render('login', {
+    user: req.user
   });
 });
 
-router.get('/login', function (req, res) {
-  res.send('Hello<br><a href="/auth">Log in with Github</a>');
-});
+// GET /auth/github
+//   Use passport.authenticate() as route middleware to authenticate the
+//   request.  The first step in GitHub authentication will involve redirecting
+//   the user to github.com.  After authorization, GitHub will redirect the user
+//   back to this application at /auth/github/callback
+router.get('/auth/github',
+  passport.authenticate('github', { scope: [ 'user:email' ] }),
+  function (req, res) {
+    // The request will be redirected to GitHub for authentication, so this
+    // function will not be called.
+  }
+);
+
+// GET /auth/github/callback
+//   Use passport.authenticate() as route middleware to authenticate the
+//   request.  If authentication fails, the user will be redirected back to the
+//   login page.  Otherwise, the primary route function will be called,
+//   which, in this example, will redirect the user to the home page.
+router.get('/auth/github/callback',
+  passport.authenticate('github', { failureRedirect: '/login' }),
+  function (req, res) {
+    res.send(req.user);
+  }
+);
 
 router.get('/api/', function (req, res) {
   res.sendFile(path.join(__dirname, '../../public/api.html'));
@@ -94,9 +54,8 @@ router.get('/api/resources/:page?', function (req, res, next) {
   let page = 1;
 
   db.Resource.findAndCountAll({
-    include: [db.Tag],
+    include: [db.Tag]
   }).then(function (data) {
-
     if (req.params.page) {
       page = req.params.page;
     }
@@ -105,21 +64,21 @@ router.get('/api/resources/:page?', function (req, res, next) {
     offset = limit * (page - 1);
 
     db.Resource.findAll({
-        limit: limit,
-        offset: offset,
-        $sort: {
-          id: 1
-        }
-      })
-      .then((resources) => {
-        res.status(200).json({
-          'result': resources,
-          'count': data.count,
-          'pages': pages
-        });
-      }).catch(function (error) {
-        res.status(500).send('Internal Server Error');
+      limit: limit,
+      offset: offset,
+      $sort: {
+        id: 1
+      }
+    }).then((resources) => {
+      res.status(200).json({
+        'result': resources,
+        'count': data.count,
+        'pages': pages
       });
+    }).catch(function (error) {
+      if (error) throw error;
+      res.status(500).send('Internal Server Error');
+    });
   });
 });
 
@@ -167,3 +126,13 @@ router.get('/api/users/:id', function (req, res) {
     res.json(dbUsers);
   });
 });
+
+// Simple route middleware to ensure user is authenticated.
+//   Use this route middleware on any resource that needs to be protected.  If
+//   the request is authenticated (typically via a persistent login session),
+//   the request will proceed.  Otherwise, the user will be redirected to the
+//   login page.
+// function ensureAuthenticated (req, res, next) {
+//   if (req.isAuthenticated()) { return next(); }
+//   res.redirect('/login');
+// }
--- server/config/express.js
+++ server/config/express.js
@@ -1,13 +1,13 @@
-var express = require('express');
-var glob = require('glob');
-
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-var compress = require('compression');
-var methodOverride = require('method-override');
-var exphbs = require('express-handlebars');
+const express = require('express');
+const session = require('express-session');
+const glob = require('glob');
+const logger = require('morgan');
+const bodyParser = require('body-parser');
+const compress = require('compression');
+const methodOverride = require('method-override');
+const exphbs = require('express-handlebars');
+const GitHubStrategy = require('passport-github2').Strategy;
+const passport = require('passport');
 
 module.exports = function (app, config) {
   var env = process.env.NODE_ENV || 'development';
@@ -28,10 +28,50 @@ module.exports = function (app, config) {
   app.use(bodyParser.urlencoded({
     extended: true
   }));
-  app.use(cookieParser());
+
   app.use(compress());
   app.use(express.static(config.root + '/public'));
   app.use(methodOverride());
+  app.use(session({ secret: 'keyboard cat', resave: false, saveUninitialized: false }));
+  // Passport session setup.
+  //   To support persistent login sessions, Passport needs to be able to
+  //   serialize users into and deserialize users out of the session.  Typically,
+  //   this will be as simple as storing the user ID when serializing, and finding
+  //   the user by ID when deserializing.  However, since this example does not
+  //   have a database of user records, the complete GitHub profile is serialized
+  //   and deserialized.
+  passport.serializeUser(function (user, done) {
+    done(null, user);
+  });
+
+  passport.deserializeUser(function (obj, done) {
+    done(null, obj);
+  });
+
+  // Use the GitHubStrategy within Passport.
+  //   Strategies in Passport require a `verify` function, which accept
+  //   credentials (in this case, an accessToken, refreshToken, and GitHub
+  //   profile), and invoke a callback with a user object.
+  passport.use(new GitHubStrategy({
+    clientID: process.env.GH_CLIENT_ID,
+    clientSecret: process.env.GH_SECRET,
+    callbackURL: '/auth/github/callback'
+  }, function (accessToken, refreshToken, profile, done) {
+    // asynchronous verification, for effect...
+    process.nextTick(function () {
+      // To keep the example simple, the user's GitHub profile is returned to
+      // represent the logged-in user.  In a typical application, you would want
+      // to associate the GitHub account with a user record in your database,
+      // and return that user instead.
+      console.log('passport callback function fired');
+      return done(null, profile);
+    });
+  }));
+
+  // Initialize Passport!  Also use passport.session() middleware, to support
+  // persistent login sessions (recommended).
+  app.use(passport.initialize());
+  app.use(passport.session());
 
   var controllers = glob.sync(config.root + '/app/controllers/*.js');
   controllers.forEach(function (controller) {
