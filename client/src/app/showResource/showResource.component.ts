0d213aa260cdde4e7b9a5e707b1edcc3e80f7f36
Tyler Schade <tylerschade99@gmail.com>
2017-11-16 21:49:29 -0500

    Building pagination

--- client/src/app/showResource/showResource.component.ts
+++ client/src/app/showResource/showResource.component.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, OnInit } from '@angular/core';
 import { ResourceService } from '../shared/resource.service';
 import { Resource } from '../shared/resource';
+import { PaginationService } from '../shared/pagination.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-show-resource',
@@ -8,15 +10,39 @@ import { Resource } from '../shared/resource';
   styleUrls: ['./showResource.component.css']
 })
 
-export class ShowResourceComponent {
+export class ShowResourceComponent implements OnInit {
   public resources: Array<Resource> = [];
+  public page: Number;
 
-  constructor (private resourceService: ResourceService) {
+  constructor(
+    private resourceService: ResourceService,
+    private paginationService: PaginationService,
+    private activatedRoute: ActivatedRoute) {
+      const pageNumber = activatedRoute.snapshot.params['id'] ?
+        activatedRoute.snapshot.params['id'] : 1;
+
+      if (this.paginationService.getCurrentPage() !== pageNumber) {
+        this.paginationService.updateCurrentPage(pageNumber);
+      }
+
+      this.downloadResources();
+    }
+
+  public ngOnInit() {
+    this.paginationService
+      .listenForPageUpdate()
+      .subscribe(pageNumber => this.handlePageUpdate(pageNumber));
+  }
+
+  public handlePageUpdate(pageNumber) {
+    this.page = pageNumber;
     this.downloadResources();
   }
 
   private downloadResources(): void {
-    this.resourceService.getAllResources().subscribe(data => {
+    this.resourceService.getAllResources(this.page).subscribe(data => {
+      this.resources = [];
+
       data.result.forEach(resourceData => this.resources.push(
         new Resource(
           resourceData.id,
